5dc151f29d191c0e8e1f69d13c3289092a08d55a
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-03-04 13:22:31 +0545

    started learning typescript

---
+++ variableMe.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// String
+var greeting = "Hello Bipin";
+greeting.toLowerCase();
+console.log(greeting);
+// Number 
+// let num: number = 3556; /* this way we can to this but not a good practice */
+/* Rather we can do this, as we know typescript checks the type of variable*/
+var num = 5588;
+num.toFixed();
+console.log(num);
+// Boolean 
+var isTrue = true;
+console.log(isTrue);
---
+++ variableMe.ts
@@ -0,0 +1,17 @@
+// String
+let greeting: string = "Hello Bipin";
+greeting.toLowerCase();
+console.log(greeting);
+
+// Number 
+// let num: number = 3556; /* this way we can to this but not a good practice */
+/* Rather we can do this, as we know typescript checks the type of variable*/
+let num = 5588;
+num.toFixed();
+console.log(num);
+
+// Boolean 
+let isTrue = true;
+console.log(isTrue);
+
+export {}
